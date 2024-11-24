eeb960efba1ce4306b2ac99a92e381d239e929eb
Bruno <bruno1022@gmail.com>
2024-11-24 15:53:07 -0300

    reactive streams

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -14,21 +14,23 @@ public class User implements Subscriber<String>{
 
     @Override
     public void onSubscribe(Subscription subscription) {
+        System.out.println("onSubscribe("+name+")");
+        subscription.request(Long.MAX_VALUE);
 
     }
 
     @Override
-    public void onNext(String s) {
-
+    public void onNext(String video) {
+        System.out.println("onNext("+video+")");
     }
 
     @Override
-    public void onError(Throwable throwable) {
-
+    public void onError(Throwable error) {
+        System.out.println("onError: "+error);
     }
 
     @Override
     public void onComplete() {
-
+        System.out.println("onComplete()");
     }
 }
--- src/main/java/org/example/YotubeSubscription.java
+++ src/main/java/org/example/YotubeSubscription.java
@@ -6,12 +6,31 @@ import org.reactivestreams.Subscription;
 import java.util.List;
 
 public class YotubeSubscription implements Subscription {
+
+    private Subscriber<? super String> subscriber;
+    private List<String> videos;
+
     public YotubeSubscription(Subscriber<? super String> subscriber, List<String> videos) {
+        this.subscriber = subscriber;
+        this.videos = videos;
     }
 
     @Override
-    public void request(long l) {
+    public void request(long size) {
+        System.out.println("request(unbounded)");
+        long requestNumberItems = getRequestNumberItems(size);
 
+        for(int i = 0; i < requestNumberItems; i++) {
+            subscriber.onNext(videos.get(i));
+        }
+        subscriber.onComplete();
+    }
+
+    private long getRequestNumberItems(long size) {
+        if (size <= videos.size()) {
+            return size;
+        }
+        return videos.size();
     }
 
     @Override
